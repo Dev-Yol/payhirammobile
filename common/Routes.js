d932ccc086556122e4b68bb3bd124c9c4fc36430
keenanmendiola <keenanpaulmendiola@gmail.com>
2020-12-20 20:21:49 +0800

    Routes: Added route for Circle

--- common/Routes.js
+++ common/Routes.js
@@ -53,4 +53,6 @@ export default {
   couponsValidate: apiUrl + 'coupons/validate',
   productsRetrieve: apiUrl + 'products/retrieve',
   productsRetrieveBasic: apiUrl + 'products/retrieve_basic',
+  circleRetrieve: apiUrl + 'my_circle/retrieve',
+  circleCreate: apiUrl + 'my_circle/create',
 };
