450137219f998a8d79df78f6023839c0cf3c51f5
kennck <kennettecanales@gmail.com>
2021-02-11 19:17:04 +0800

    update

--- modules/circle/index.js
+++ modules/circle/index.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import { View, Text, ScrollView, TouchableHighlight } from 'react-native';
+import { View, Text, ScrollView, TouchableHighlight, Dimensions } from 'react-native';
+const height = Math.round(Dimensions.get('window').height);
 import { UserImage, Spinner } from 'components';
 import { Rating } from 'components/index.js';
 import { Routes, Color } from 'common';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faTimesCircle, faEllipsisH} from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
 import Api from 'services/api/index.js';
 class Circle extends Component{
   constructor(props){
     super(props);
     this.state = {
-      circles: [],
+      data: null,
       isLoading: true
     }
   }
@@ -22,32 +23,43 @@ class Circle extends Component{
     Api.request(Routes.circleRetrieve, {account_id: user.account_id}, response => {
       this.setState({isLoading: false})
       if(response.data != null){
-        this.setState({circles: response.data});
+        this.setState({data: response.data});
+      }else{
+        this.setState({data: null})
       }
     });
   }
 
-  renderCircles() {
+
+  renderCircles(data) {
     return (
-      this.state.circles.map((el, ndx) => {
+      data.map((item, index) => {
+        console.log('item', item.account.status)
         return (
           <TouchableHighlight 
             onPress={() => {
-              this.redirect(el)
+              this.redirect(item)
             }}
             underlayColor={Color.gray}
-            key={ndx}
+            key={index}
           >
-            <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
-              <UserImage user={el.account} style={[{flex: 3}]}/>
-              <View style={[{flex: 3, marginLeft: 5}]}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <FontAwesomeIcon icon={ faCheckCircle } style={{color: 'blue', marginRight: 5}} size={15} />
-                  <Text style={[{fontWeight: 'bold', margin: 2}]}>{el.account.username}</Text>
-                </View>
-                <Text style={[{margin: 2}]}>{el.account.information.address != null ? el.account.information.address : "---<>---"}</Text>
+            <View style={{
+              flexDirection: 'row',
+              paddingTop: 10,
+              paddingBottom: 10,
+              width: '90%',
+              marginLeft: '5%',
+              maginRight: '5%',
+              alignItems: 'center'
+            }}>
+              <UserImage user={item.account}/>
+              <View style={{
+                  marginLeft: 5,
+                  alignItems: 'center'
+                }}>
+                <Text style={{fontWeight: 'bold'}}>{item.account.username}</Text>
+                <Text style={[{margin: 2}]}>{item.account.information.address}</Text>
               </View>
-              <Rating ratings={el.rating} style={[{flex: 3}]}></Rating>
             </View>
           </TouchableHighlight>
         )
@@ -59,16 +71,20 @@ class Circle extends Component{
     this.props.navigation.push("viewProfileStack", { user })
   }
   render() {
+    const { data} = this.state;
     return (
       <View style={
         {
-          paddingTop: 70,
-          minHeight: '100%'
+          marginTop: 70,
+          flex: 1
         }
       }>
-        <ScrollView showsHorizontalScrollIndicator={false}>
-          {this.renderCircles()}
-          
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{
+            height: height
+          }}>
+            {data && this.renderCircles(data)}
+          </View>
         </ScrollView>
         {this.state.isLoading ? <Spinner mode="overlay"/> : null }
       </View>
