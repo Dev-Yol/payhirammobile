42eb547eec00d59c204be0b6bad6eb1ac27c6c1e
BargasoRC <renanbargaso16@gmail.com>
2021-02-11 20:27:18 +0800

    tranfer fund card initial

--- modules/generic/TransferFundCard.js
+++ modules/generic/TransferFundCard.js
@@ -1,30 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, Image } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, Image, TouchableHighlight } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faStar as Solid } from '@fortawesome/free-solid-svg-icons';
+import {faStar as Regular} from '@fortawesome/free-regular-svg-icons';
 import styles from './TransferFundStyle.js';
 import {connect} from 'react-redux';
-import {BasicStyles} from 'common';
+import {BasicStyles, Color} from 'common';
 
 class TransferFundCard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedStar: null
+    }
+  }
   
+  renderStars = () => {
+    const starsNumber = [1, 2, 3, 4, 5];
+    return starsNumber.map((star, index) => {
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({selectedStar: index + 1});
+          }}
+          key={index}
+          style={styles.StarContainer}>
+          <FontAwesomeIcon
+            color={'#FFCC00'}
+            icon={Solid}
+            size={BasicStyles.iconSize}
+            style={{
+              color: '#FFCC00',
+            }}
+          />
+        </TouchableOpacity>
+      )
+    })
+  }
+
   render() {
-    console.log('\n\n=============================================================', this.props.state.user, '\n\n===============================================================')
-    const {user} = this.props.state
+    const {user, theme} = this.props.state
     return (
       <SafeAreaView>
         <View
           style={[
             {
-              borderColor: 'black',
-              borderWidth: 1,
               paddingTop: 20,
               paddingBottom: 20,
               alignItems: 'center'
             }
           ]}
         >
-         {/* {
+         {
             user.profile != null && user.profile.url != null && (
               <Image
                 source={{uri: Config.BACKEND_URL  + user.profile.url}}
@@ -35,14 +62,43 @@ class TransferFundCard extends Component {
             (user.profile == null || (user.profile != null && user.profile.url == null)) && (
               <FontAwesomeIcon
                 icon={faUserCircle}
-                size={BasicStyles.profileIconSize}
+                size={BasicStyles.profileIconSize + 30}
                 style={{
-                  color: this.props.color ? this.props.color : Color.primary
+                  color: theme ? theme.primary : Color.primary
                 }}
               />
             )
-          } */}
-          <Text> Full Name </Text>
+          }
+          <Text style={{
+            marginTop: 10,
+            color: theme ? theme.primary : Color.primary
+          }}> Full Name </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center'
+          }}
+        >
+          {
+            this.renderStars()
+          }
+        </View>
+        <View
+          style={{
+            flexDirection: 'row'
+          }}
+        >
+          <TouchableHighlight>
+            <Text>
+              CANCEL
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight>
+            <Text>
+              CONTINUE
+            </Text>
+          </TouchableHighlight>
         </View>
       </SafeAreaView>
     )
