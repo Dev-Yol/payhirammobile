835bbb0bcebf7d39b8c78b0561999cae2a061ab2
= <=>
2021-06-12 18:57:22 +0800

    update

--- modules/generic/SecurityAlert.js
+++ modules/generic/SecurityAlert.js
@@ -21,49 +21,84 @@ function SecutiryAlert(props) {
     
   }
 
+  console.log(props.params)
   return (
     <View style={{
-      flexDirection: 'row',
-      width: '100%',
-      justifyContent: 'space-between',
-      alignItems: 'space-between'
+      width: '100%'
     }}>
-      <Button
-        onClick={() => {
-          deAuthenticate()
-        }}
-        title={'Logout'}
-        style={{
-          borderColor: Color.danger,
-          borderWidth: 1,
-          width: '45%',
-          height: 50,
-          borderRadius: 25,
-          backgroundColor: 'transparent'
-        }}
-        textStyle={{
-          color: Color.danger,
-          fontSize: BasicStyles.standardFontSize
-        }}
-      />
+    {
+      props.params == 'recover' && (
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'space-between',
+          width: '100%'
+        }}>
+          <Button
+            onClick={() => {
+              deAuthenticate()
+            }}
+            title={'Logout'}
+            style={{
+              borderColor: Color.danger,
+              borderWidth: 1,
+              width: '45%',
+              height: 50,
+              borderRadius: 25,
+              backgroundColor: 'transparent'
+            }}
+            textStyle={{
+              color: Color.danger,
+              fontSize: BasicStyles.standardFontSize
+            }}
+          />
 
-
-      <Button
-        onClick={() => {
-          props.resetInactivityTimeout()
-        }}
-        title={"Continue using"}
-        style={{
-          backgroundColor: props.theme ? props.theme.secondary : Color.secondary,
-          width: '45%',
-          height: 50,
-          borderRadius: 25,
-        }}
-        textStyle={{
-          color: Color.white,
-          fontSize: BasicStyles.standardFontSize
-        }}
-      />
+          <Button
+          onClick={() => {
+            props.resetInactivityTimeout()
+          }}
+          title={"Continue using"}
+          style={{
+            backgroundColor: props.theme ? props.theme.secondary : Color.secondary,
+            width: '45%',
+            height: 50,
+            borderRadius: 25,
+          }}
+          textStyle={{
+            color: Color.white,
+            fontSize: BasicStyles.standardFontSize
+          }}
+        />
+        </View>
+      )
+    }
+    {
+      props.params == 'auto' && (
+        <View style={{
+          width: '100%',
+          alignItems: 'center'
+        }}>
+          <Button
+            onClick={() => {
+              deAuthenticate()
+            }}
+            title={'Logout'}
+            style={{
+              borderColor: Color.danger,
+              borderWidth: 1,
+              width: '45%',
+              height: 50,
+              borderRadius: 25,
+              backgroundColor: 'transparent'
+            }}
+            textStyle={{
+              color: Color.danger,
+              fontSize: BasicStyles.standardFontSize
+            }}
+          />
+        </View>
+      )
+    }
     </View>
   );
 }
