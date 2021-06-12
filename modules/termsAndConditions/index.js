f18cac470fb28cb7ae18a6c3d81370130cf07b69
= <=>
2021-06-12 17:14:36 +0800

    update

--- modules/termsAndConditions/index.js
+++ modules/termsAndConditions/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { View } from 'react-native'
 import { WebView } from 'react-native-webview';
 
 class TermsAndConditions extends Component {
   render() {
     return (
-      <WebView source={{ uri: 'https://payhiram.ph/terms-and-conditions' }} />
+      <View style={{
+        flex: 1
+      }}>
+        <WebView
+          source={{ uri: 'https://payhiram.ph/terms-and-conditions' }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={false}
+          scalesPageToFit={true} />
+      </View>
     );
   }
 }
